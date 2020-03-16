063ac41e2df68eaa0278b2ccf85baaae1f32db1f
zhangyue <zhangyue@beisen.com>
2020-03-16 16:51:17 +0800

    peer real

--- js/main-call-offer.js
+++ js/main-call-offer.js
@@ -32,27 +32,22 @@ showView().then(stream => {
   // 本地采集的媒体流添加到Peer
   stream.getTracks().forEach(track => peerConnection.addTrack(track, stream))
 
-  socket.on('user-joined-number', userNumber => {
-    console.log('User Joined Number: ' + userNumber)
-    if (userNumber > 1) {
-      // 创建请求端，offerOpts获取本地媒体参数信息
-      // Peer通讯媒体信息交换
-      peerConnection.createOffer({
-        'offerToRecieveAudio': 0,
-        'offerToRecieveVideo': 1
-      })
-        .then(offer => {
-          console.log(`Send Offer`, offer)
-
-          peerConnection.setLocalDescription(offer)
-          // 请求方准备好媒体信息后，发送给接受方
-          socket.emit('send-offer', offer)
-        })
-        .catch(err =>
-          console.log(`failed to create offer: ${err}`)
-        )
-    }
+  // 创建请求端，offerOpts获取本地媒体参数信息
+  // Peer通讯媒体信息交换
+  peerConnection.createOffer({
+    'offerToRecieveAudio': 0,
+    'offerToRecieveVideo': 1
   })
+    .then(offer => {
+      console.log(`Send Offer`, offer)
+
+      peerConnection.setLocalDescription(offer)
+      // 请求方准备好媒体信息后，发送给接受方
+      socket.emit('send-offer', offer)
+    })
+    .catch(err =>
+      console.log(`failed to create offer: ${err}`)
+    )
 })
 
 $("#hangup").on('click', function (e) {
